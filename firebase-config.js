bf545104c2fc32e6a4604a5bd5347bc92916c735
rvpiffer-dev <rvpiffer@gmail.com>
2026-08-21 08:58:03 -0300

    Adiciona configuração Firebase

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Configuração pública do Firebase Web App
+const firebaseConfig = {
+  apiKey: "AIzaSyDuEvZt8yvQF1OoiQoeYd9YSjiZPc1Wjgg",
+  authDomain: "controle-obra-casa-a3d59.firebaseapp.com",
+  projectId: "controle-obra-casa-a3d59",
+  storageBucket: "controle-obra-casa-a3d59.firebasestorage.app",
+  messagingSenderId: "157648632089",
+  appId: "1:157648632089:web:9e9fef4de3cd6dd451ad88",
+  measurementId: "G-5DP3S05P8R"
+};
